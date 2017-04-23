39f9d0a887119f5278914f275d0889487fffa77e
Akin909 <akin.sowemimo@gmail.com>
2017-04-23 16:26:21 +0100

    Add react.children and clone elements methods [wip]

--- src/components/formatter.js
+++ src/components/formatter.js
@@ -1,51 +1,23 @@
 import React from 'react';
-import uuid from 'uuid/v4';
 import { Hour, Second, Minute, Separator, Ampm } from './time';
 
-export const Formatter = props => {
-  let children = props.format.split('').map((e, index) => {
+export const Formatter = ({ format, state }) => {
+  let children = format.split('').map((e, index) => {
     if (e === 'h') {
-      return <div><Hour key={uuid()} {...props} /><span>h</span></div>;
+      return <Hour />;
     } else if (e === 'm') {
-      return <div key={uuid()}><Minute {...props} /><span> mins</span></div>;
+      return <Minute />;
     } else if (e === 's') {
-      return <div key={uuid()}><Second {...props} /><span> secs</span></div>;
+      return <Second />;
     } else if (e === 'p') {
-      return <Ampm key={uuid()} {...props} />;
-    } else if (e === 'T' || e === 'M') {
-      return (
-        <div key={uuid()}>
-          <Hour {...props} />
-          <Separator {...props} />
-          <Minute {...props} />
-          <Separator {...props} />
-          <Second {...props} /> {e === 'M' ? '' : <Ampm {...props} />}
-        </div>
-      );
+      return <Ampm />;
+    } else if (e === ' ') {
+      <span />;
     }
   });
-  return <span>{children}</span>;
+  return (
+    <div>
+      {React.Children.map((children, c) => React.cloneElement(c, state))}
+    </div>
+  );
 };
-// TODO Refactor ?with a switch statement
-// switch (e) {
-//   case 'h':
-//     return <div><Hour key={uuid()} {...props} /><span>h</span></div>;
-//   case 'm':
-//     return <div key={uuid()}><Minute {...props} /><span> mins</span></div>;
-//   case 's':
-//     return <div key={uuid()}><Second {...props} /><span> secs</span></div>;
-//   case 'p':
-//     return <Ampm key={uuid()} {...props} />;
-//   case 'M' || 'T':
-//     return (
-//       <div key={uuid()}>
-//         <Hour {...props} />
-//         <Separator {...props} />
-//         <Minute {...props} />
-//         <Separator {...props} />
-//         <Second {...props} /> {e === 'M' ? '' : <Ampm {...props} />}
-//         </div>
-//         );
-//         default:
-
-// }
